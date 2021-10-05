46db40b38aba5fa5f7e84f3cc04fb1628668dfba
Anant Gupta <61108547+anant-creator@users.noreply.github.com>
2021-10-05 10:42:25 +0530

    new js

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('.btn');
+const cont = document.querySelector('.section-body');
+
+
+button.addEventListener('click', function() {
+  cont.style.background = "white";
+})
